711bda58e7961c26961463be6fc755bdcc34967a
klelia <clelia.fradella@gmail.com>
2023-05-23 13:12:14 +0200

    add home controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'home');
+Route::get('/', [HomeController::class, 'home'])->name('home');
 
 Route::get('/books', [BookController::class, 'index'])->name('books.index');
 

e26f665f904ea0e65a986fa8f308c03370f0869b
klelia <clelia.fradella@gmail.com>
2023-05-24 17:09:34 +0200

    rem

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,7 +21,6 @@
             optio earum. Ea, repudiandae odit! Eum animi corporis sequi iure quas delectus et, beatae labore rem, repellat
             officiis est libero exercitationem ut sit possimus tempore, pariatur culpa alias consequuntur. Quibusdam totam
             tempore expedita nemo harum ipsum.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quidem consectetur in illum pariatur rerum ut,
-            ipsam similique explicabo mollitia.</p>
+
     </section>
 @endsection
